c3010c66dc2f18950a5e14843d8b56ccf2678954
Adrian-Developer <pablo.huaxteco@gmail.com>
2017-03-06 00:41:16 -0600

    Add files via upload

---
+++ Views/CatsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Cats
+{
+    public partial class CatsPage : ContentPage
+    {
+        public CatsPage()
+        {
+            InitializeComponent();
+            ListViewCats.ItemSelected += ListViewCats_ItemSelected;
+        }
+
+        private async void ListViewCats_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var SelectedCat = e.SelectedItem as Models.Cat;
+            if (SelectedCat!=null)
+            {
+                await Navigation.PushAsync(new DetailsPage(SelectedCat));
+                ListViewCats.SelectedItem = null;
+            }
+        }
+    }
+}
